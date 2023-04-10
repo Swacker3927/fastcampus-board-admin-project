552f4e06b5d2abc8fcb741175a6958f2eb49ce41
lee31 <sai6321@naver.com>
2023-04-10 23:58:49 +0900

    임포트 정리

--- src/main/java/com/fastcampus/boardadminproject/config/SecurityConfig.java
+++ src/main/java/com/fastcampus/boardadminproject/config/SecurityConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.security.config.Customizer.withDefaults;
+import static org.springframework.security.config.Customizer.*;
 
 @Configuration
 public class SecurityConfig {
