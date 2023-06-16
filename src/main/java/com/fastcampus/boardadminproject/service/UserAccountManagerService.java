b298578377ab7fe2718816537a5f51941041dab7
lee31 <sai6321@naver.com>
2023-06-16 20:46:33 +0900

    #40 - 회원 관리 비즈니스 로직 및 컨트롤러 테스트 작성

---
+++ src/main/java/com/fastcampus/boardadminproject/service/UserAccountManagerService.java
@@ -0,0 +1,24 @@
+package com.fastcampus.boardadminproject.service;
+
+import com.fastcampus.boardadminproject.dto.UserAccountDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class UserAccountManagerService {
+
+    public List<UserAccountDto> getUserAccounts() {
+        return List.of();
+    }
+
+    public UserAccountDto getUserAccount(String userId) {
+        return null;
+    }
+
+    public void deleteUserAccount(String userId) {
+
+    }
+}
