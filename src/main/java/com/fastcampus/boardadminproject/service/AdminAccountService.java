4f49503bc5cdb7500f13ba0f1ed780fd87f077e9
lee31 <sai6321@naver.com>
2023-07-08 22:51:47 +0900

    #50 - 어드민 회원 서비스 코드 작성과 테스트

---
+++ src/main/java/com/fastcampus/boardadminproject/service/AdminAccountService.java
@@ -0,0 +1,35 @@
+package com.fastcampus.boardadminproject.service;
+
+import com.fastcampus.boardadminproject.domain.constant.*;
+import com.fastcampus.boardadminproject.dto.AdminAccountDto;
+import com.fastcampus.boardadminproject.repository.AdminAccountRepository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+@RequiredArgsConstructor
+@Service
+public class AdminAccountService {
+
+    private final AdminAccountRepository adminAccountRepository;
+
+    public Optional<AdminAccountDto> searchUser(String username) {
+        return Optional.empty();
+    }
+
+    public AdminAccountDto saveUser(String username, String password, Set<RoleType> roleTypes, String email, String nickname, String memo) {
+        return null;
+    }
+
+    public List<AdminAccountDto> users() {
+        return List.of();
+    }
+
+    public void deleteUser(String username) {
+
+    }
+}
